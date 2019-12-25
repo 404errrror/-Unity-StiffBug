d9d2ec412d06ab0638915fd60162557695dbf7e1
404error <404error_@naver.com>
2019-12-25 22:38:11 +0900

    Added CSVReader

---
+++ Assets/Scripts/Control/MInputController.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * 2019.12.07
+ * 게임패드, 조이스틱으로 입력데이터를 받고, InputProperties를 업데이트합니다.
+ */
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MInputController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JoyStick_Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GamePad_Update();
+        JoyStick_Update();
+    }
+
+    #region GamePad
+    /* 게임 패드 조이스틱의 민감도입니다. 범위 : 0.0f ~ 1.0f */
+    /* 민감도가 너무 높으면 조이스틱을 뗄 때에도 반응하게 되어 정확도가 낮아질 수 있습니다. */
+    public float GamePad_Sensitive = 0.75f;
+    float _GamePad_Sensitive;
+
+    void GamePad_Start()
+    {
+        const float Root2 = 1.4142135623f;
+        GamePad_Sensitive = Mathf.Clamp(GamePad_Sensitive, 0.0f, 1.0f);
+        _GamePad_Sensitive = Root2 - (GamePad_Sensitive * Root2);
+    }
+
+    void GamePad_Update()
+    {
+        Vector2 leftAxis = new Vector2(Input.GetAxis("Left X Axis"), Input.GetAxis("Left Y Axis"));
+        Vector2 rightAxis = new Vector2(Input.GetAxis("Right X Axis"), Input.GetAxis("Right Y Axis"));
+
+        if (leftAxis.sqrMagnitude > _GamePad_Sensitive)
+            InputProperities.Instance.SetRotationL(leftAxis);
+        if (rightAxis.sqrMagnitude > _GamePad_Sensitive)
+            InputProperities.Instance.SetRotationR(rightAxis);
+    }
+    #endregion // GamePad
+
+
+    #region JoyStick
+    public MJoyStick JoystickObj_L;
+    public MJoyStick JoystickObj_R;
+
+    void JoyStick_Start()
+    {
+        if (JoystickObj_L == null) Debug.LogError("MInputController.JoyStick_Start(), JoystickObj_L is null");
+        if (JoystickObj_R == null) Debug.LogError("MInputController.JoyStick_Start(), JoystickObj_R is null");
+    }
+
+    void JoyStick_Update()
+    {
+        if(JoystickObj_L) InputProperities.Instance.SetRotationL(JoystickObj_L.GetStickPosition());
+        if(JoystickObj_R) InputProperities.Instance.SetRotationR(JoystickObj_R.GetStickPosition());
+    }
+    #endregion // TouchPad JoyStick
+}
--- Assets/Scripts/Control/MJoyStick.cs
+++ Assets/Scripts/Control/MJoyStick.cs
@@ -7,16 +7,9 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public enum EJoyStickControl
-{
-    JoyStickControl_Left = 0,
-    JoyStickControl_Right
-}
-
 public class MJoyStick : MonoBehaviour
 {
     public GameObject stick;
-    public EJoyStickControl joyStickControl;
 
     void Start()
     {
@@ -27,12 +20,12 @@ public class MJoyStick : MonoBehaviour
         {
             EventTrigger.Entry entry = new EventTrigger.Entry();
             entry.eventID = EventTriggerType.PointerEnter;
-            entry.callback.AddListener((data) => { UpdateVector(((PointerEventData)data).position); });
+            entry.callback.AddListener((data) => { UpdateStickPosition(((PointerEventData)data).position); });
             eventTrigger.triggers.Add(entry);
 
             entry = new EventTrigger.Entry();
             entry.eventID = EventTriggerType.Drag;
-            entry.callback.AddListener((data) => { UpdateVector(((PointerEventData)data).position); });
+            entry.callback.AddListener((data) => { UpdateStickPosition(((PointerEventData)data).position); });
             eventTrigger.triggers.Add(entry);
         }
         else
@@ -42,16 +35,15 @@ public class MJoyStick : MonoBehaviour
             Debug.LogError("MJoyStick.Start(), Stick is null!!");
     }
 
-    public void UpdateVector(Vector2 position)
+    private void UpdateStickPosition(Vector2 position)
     {
         /* Stick 위치 업데이트 */
         stick.transform.position = position;
+    }
 
-        /* InputManager 데이터 업데이트 */
-        switch (joyStickControl)
-        {
-            case EJoyStickControl.JoyStickControl_Left:    InputProperities.Instance.SetRotationL(stick.transform.localPosition); break;
-            case EJoyStickControl.JoyStickControl_Right: InputProperities.Instance.SetRotationR(stick.transform.localPosition); break;
-        }
+
+    public Vector2 GetStickPosition()
+    {
+        return stick.transform.position;
     }
 }
