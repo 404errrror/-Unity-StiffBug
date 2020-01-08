834d0c588bcb3f38700bc4ab2e147df6712bb229
404error <404error_@naver.com>
2020-01-09 00:20:17 +0900

    DataTable 구조 추가

---
+++ Assets/Scripts/DataTable/DataTableClasses.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorMenuTable : DataTableInterface<EditorMenuTable, EditorMenuTable.EditorMenuProperty>
+{
+    public struct EditorMenuProperty
+    {
+        public string alias;
+        public string name;
+        public string imageName;
+        public string parent;
+        public int blockID;
+    }
+    override protected void Init()
+    {
+        Load("Alias", "EditorMenuTable");
+    }
+
+    protected override EditorMenuProperty RowToProperty(Dictionary<string, string> row)
+    {
+        EditorMenuProperty result = new EditorMenuProperty();
+        foreach (var item in row)
+        {
+            switch (item.Key)
+            {
+                case "Alias":       result.alias = item.Value       as string;          break;
+                case "Name":        result.name = item.Value        as string;          break;
+                case "ImageName":   result.imageName = item.Value   as string;          break;
+                case "Parent":      result.parent = item.Value      as string;          break;
+                case "BlockID":     int.TryParse(item.Value, out result.blockID);       break;
+
+                default:
+                    Debug.LogError("DataTableClasses.cs, EditorMenuTable, <" + item.Key + ">의 데이터 변환이 실패하였습니다!!");
+                    break;
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/DataTable/DataTableInterface.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DataTableInterface<T, PropertyType> : Singleton<T>  where T : class
+{
+    private bool isInit;
+    private Dictionary<string, PropertyType> dataTable;
+
+    public void _Init()
+    {
+        isInit = true;
+        dataTable = new Dictionary<string, PropertyType>();
+
+        Init();
+    }
+
+    protected abstract void Init();
+    protected abstract PropertyType RowToProperty(Dictionary<string, string> row);
+
+    public PropertyType GetProperty(string alias)
+    {
+        if(isInit == false)
+        {
+            Debug.LogError("Please Initialize!!");
+        }
+
+        if(dataTable.ContainsKey(alias) == false)
+        {
+            Debug.LogError("DataTableInterface.cs, " + alias + "에 해당되는 Key가 존재하지 않습니다!!");
+        }
+
+        return dataTable[alias];
+    }
+
+    protected void Load(string alias, string path)
+    {
+        List<Dictionary<string, string>> temp = CSVReader.Read(path);
+
+        foreach (var row in temp)
+        {
+            string _alias = row[alias] as string;
+            row.Remove(alias);
+
+            dataTable.Add(_alias, RowToProperty(row));
+        }
+    }
+
+}
